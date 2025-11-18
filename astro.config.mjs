0257b8857884994098e4c53e5b1694b11a5d2031
Alan Zambrano <alnzam17@gmail.com>
2025-11-17 18:50:26 -0600

    feat: add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: vercel(),
   integrations: [react()],
 });
